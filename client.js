145ae261677bc1047e8a7497e09193aa50834067
Cara Ponzini <ponzini@gmail.com>
2019-04-09 11:35:27 -0700

    client edits

--- client.js
+++ client.js
@@ -1,7 +1,25 @@
 const net = require('net');
+const readline = require('readline'); //gives command line tool
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: '$$'
+});
 
 const client = net.createConnection(5446, 'localhost', () => {
   console.log('I am connected');
-  client.write('hello I am a client');
+  
+  rl.prompt();
+  rl.on('line', line => {
+    client.write(line);
+    rl.prompt();
+  });
+
+});
+
+client.on('data', data => {
+  console.log(`\n${data.toString()}`);
+  rl.prompt();
 });
 
--- server.js
+++ server.js
@@ -3,11 +3,14 @@ const net = require('net');
 //every client is a socket(node.js doc term), but is a particular client connecting to server.
 const server = net.createServer(client => {
   console.log('client connected');
+
+  //   client.pipe(client);
   client.on('data', data => {
-    console.log(data.toString());
+    client.write(`ECHO FROM SERVER: ${data}`);
   });
 });
 
+// tell server to listen on port no.
 server.listen(5446);
 
 
